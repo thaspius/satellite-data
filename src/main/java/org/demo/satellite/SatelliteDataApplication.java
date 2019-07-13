62bee3719c3a22e81953dab3c27187a958643871
Jarrod Sears <thaspius@gmail.com>
2019-07-13 15:36:43 -0500

    checking init project from spring boot initializr

---
+++ src/main/java/org/demo/satellite/SatelliteDataApplication.java
@@ -0,0 +1,13 @@
+package org.demo.satellite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatelliteDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatelliteDataApplication.class, args);
+	}
+
+}
